84131405ce87f7d7bde6b253e00237cf7f9b76d2
jacob <jacob@amazon.com>
2019-08-01 11:53:37 -0700

    Mimicking Python socket api

--- socket.el
+++ socket.el
@@ -1,20 +1,15 @@
 
-(defstruct socket host port (state :new)
-	   (buffer nil) (process nil) (finalizer nil))
-
+(defstruct socket host port (buffer nil) (process nil) (finalizer nil))
 
 (defun socket-close (socket)
   (unless (null (socket-process socket))
     (delete-process (socket-process socket)))
   (when (buffer-live-p (socket-buffer socket))
     (kill-buffer (socket-buffer socket))))
-  
 
 (defun socket-connect (socket)
   (setf (socket-finalizer socket) (make-finalizer #'(lambda ()
 						      (socket-close socket))))
-  (unless (eq (socket-state socket) :new)
-    (error "socket-connect: invalid state: %s" (socket-state state)))
   (let* ((buffer (generate-new-buffer " *socket*")))
       (setf (socket-buffer socket) buffer)
       (setf (socket-process socket)
@@ -31,6 +26,3 @@
   (with-current-buffer (socket-buffer socket)
     (prog1 (buffer-string)
       (erase-buffer))))
-
-  
-    
