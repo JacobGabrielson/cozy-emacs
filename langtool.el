f18b4541a36d263bfd48c98617b62a114e2c4b9a
jacob <jacob@amazon.com>
2021-08-18 14:33:44 -0700

    initial verison

---
+++ langtool.el
@@ -0,0 +1,7 @@
+;; https://dev.languagetool.org/http-server.html?utm_source=pocket_mylist
+
+;; wget https://languagetool.org/download/LanguageTool-stable.zip
+
+(load-file "~/Documents/GitHub/Emacs-langtool/langtool.el")
+(setq langtool-language-tool-jar "/home/ANT.AMAZON.COM/jacob/src/LanguageTool-5.4/languagetool-commandline.jar")
+(require 'langtool)
