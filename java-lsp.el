7f09a17b2b05e6741dd512528f841e6fd8e9243b
Jacob Gabrielson <jacobg23@pobox.com>
2022-08-15 10:57:56 -0700

    add some useful docs on where I found sample .emacs

--- java-lsp.el
+++ java-lsp.el
@@ -4,3 +4,11 @@
 ;; need to set this
 ;; (setq lsp-java-java-path "/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin/java")
 
+
+;; add'l config to look at:
+
+;; https://github.com/MatthewZMD/.emacs.d#orgf8b7fe2
+;; https://pastebin.com/Pb9K8StS
+;; https://github.com/neppramod/emacs-configuration/blob/master/configuration.org
+;; https://github.com/neppramod/java_emacs/blob/master/emacs-configuration.org
+
