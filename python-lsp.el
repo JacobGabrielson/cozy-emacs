693a02e053364e9b749c6c0d7b08894436f0085f
Jacob Gabrielson <jacobg23@pobox.com>
2023-12-15 16:32:59 -0800

    add info on lsp-deferred issue

--- python-lsp.el
+++ python-lsp.el
@@ -4,53 +4,33 @@
 (setq lsp-pylsp-plugins-flake8-exclude '("E501"))
 
 ;;(setq lsp-pyls-plugins-flake8-exclude nil)
-(setq lsp-pylsp-plugins-flake8-exclude nil)
+;;(setq lsp-pylsp-plugins-flake8-exclude nil)
 
 ;;(setq lsp-pyls-plugins-pycodestyle-ignore '("D100"))
 ;;(setq lsp-pyls-plugins-pycodestyle-ignore '("D100" "E501"))
-(setq lsp-pylsp-plugins-pycodestyle-ignore '("D100" "E501"))
+(setq lsp-pylsp-plugins-pycodestyle-ignore '("D100" "D107" "E501"))
 
-(use-package direnv
-  :ensure t
-  :config
-  (direnv-mode))
+(setq lsp-pylsp-plugins-pydocstyle-add-ignore '("D100" "D101" "D107" "E501"))
 
-; setup Emacs path from our ~/.bashrc
-(use-package exec-path-from-shell
-  :ensure t
-  :config
-  (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize)))
+(setq lsp-pyls-plugins-pycodestyle-enabled nil)
+
+;;(setq lsp-pylsp-plugins-pydocstyle-add-ignore nil)
 
 
-; Let's set up company! perhaps not necessary but this is what i like to use
-(use-package company
-  :ensure t
-  :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1))
 
-; install lsp mode
-(use-package lsp-mode
-  :ensure t
-  :hook (python-mode . lsp-deferred)
-  :commands (lsp lsp-deferred))
-
-; also installs lsp as a dependency
-(use-package lsp-ui
-  :ensure t
-  :hook (lsp-mode . lsp-ui-mode))
-
-
-(require 'dap-python)
+;;(require 'dap-python)
 ;; if you installed debugpy, you need to set this
 ;; https://github.com/emacs-lsp/dap-mode/issues/306
-(setq dap-python-debugger 'debugpy)
-(setq dap-python-executable "python3")
+;;(setq dap-python-debugger 'debugpy)
+;;(setq dap-python-executable "python3")
 
 (add-hook 'python-mode-hook 'indent-guide-mode)
 
 (defun pm-display-line-numbers ()
+  (direnv-update-directory-environment)
   (setq display-line-numbers t))
 
 (add-hook 'python-mode-hook 'pm-display-line-numbers)
+;; See https://github.com/emacs-lsp/lsp-mode/pull/3975 for why this
+;; might not always work?
+(add-hook 'python-mode-hook 'lsp-deferred)
