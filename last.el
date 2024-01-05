b17f05100f4e867ae6aae84763c6d1a158d8ecda
Jacob Gabrielson <jacobg23@pobox.com>
2024-01-05 10:31:50 -0800

    new file to be included last

---
+++ last.el
@@ -0,0 +1,11 @@
+;; always include last in your ~/.emacs
+
+;; Seems to be more actively maintained?
+(use-package envrc
+  :init
+  (envrc-global-mode))
+
+;;(use-package direnv :ensure t
+;;  :config
+;;  (direnv-mode))
+
